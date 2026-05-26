24bcf83f985849c1e0523eb2e303fbb129159438
mrshaikhmuhammad <mrshaikhmuhammed@gmail.com>
2026-05-27 01:00:32 +0500

    reorganized move piece function

--- src/main/java/com/muhammad/chess/entity/Board.java
+++ src/main/java/com/muhammad/chess/entity/Board.java
@@ -15,42 +15,40 @@ public class Board {
 
     }
 
-    public void move(int oldRow, int oldCol, int newRow, int newCol){
-        if (checkEmpty(newRow, newCol)) {
-            board[newRow][newCol] = board[oldRow][oldCol];
-            board[oldRow][oldCol] = null;
-//            deadPeices.add(board[newRow][newCol]);
-        }
-        else if(board[oldRow][oldCol].checkKill(newRow, newCol, this)){
-
-        }
+    public void movePiece(int oldRow, int oldCol, int newRow, int newCol){
         try {
             // is location in the box
-            if (checkLocation(newRow, newCol) && checkLocation(oldRow, oldCol)) {
-                // is that box empty to move
-                if (checkEmpty(newRow, newCol)) {
-                    board[newRow][newCol] = board[oldRow][oldCol];
-                    board[oldRow][oldCol] = null;
-                }
-                // do I need to kill enemy to move their
-                else if(board[oldRow][oldCol].checkEnemy(board[newRow][newCol])){
-                    deadPeices.add(board[newRow][newCol]);
-                    board[newRow][newCol] = board[oldRow][oldCol];
-                    board[oldRow][oldCol] = null;
-                }
-                // it's your friend sitting their
-                else{
-                    throw new Exception("Invalid location movement\nTrying to kill your friend");
-                }
+            if (!checkLocation(newRow, newCol) || !checkLocation(oldRow, oldCol)) {
+                throw new Exception("Location out of board");
+            }
 
-              // location is out of box
-            } else {
-                throw new Exception("Invalid location movement");
+            // validate piece exists
+            if (checkEmpty(oldRow, oldCol)) {
+                throw new Exception("No piece at source location");
+            }
+
+            if(board[oldRow][oldCol] instanceof  Pawn){
+                ((Pawn) board[oldRow][oldCol]).setInitial(false);
+            }
+
+            // is that box empty to move
+            if (checkEmpty(newRow, newCol)) {
+                board[newRow][newCol] = board[oldRow][oldCol];
+                board[oldRow][oldCol] = null;
+            }
+            // do I need to kill enemy to move their
+            else if(board[oldRow][oldCol].checkEnemy(board[newRow][newCol])){
+                deadPeices.add(board[newRow][newCol]);
+                board[newRow][newCol] = board[oldRow][oldCol];
+                board[oldRow][oldCol] = null;
+            }
+            // it's your friend sitting their
+            else{
+                throw new Exception("Can not kill own friend");
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
     }
 
     public void setBoard(){
