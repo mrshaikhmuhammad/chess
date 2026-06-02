001122f5cc6f0aabd42c53e01d8df6fa7dcc0ee3
mrshaikhmuhammad <mrshaikhmuhammed@gmail.com>
2026-06-02 13:50:00 +0500

    move validation, bounds check in one place

---
+++ src/main/java/com/muhammad/chess/model/Position.java
@@ -0,0 +1,48 @@
+package com.muhammad.chess.model;
+
+import java.util.Objects;
+
+public class Position {
+    private int row;
+    private int col;
+
+    public Position(int row, int col){
+        if (row >= 0 && row < 8  &&  col >= 0 && col < 8) {
+            this.row = row;
+            this.col = col;
+        }
+        else{
+            throw new IllegalArgumentException("Invalid chess position!");
+        }
+    }
+
+    public int getCol() {
+        return col;
+    }
+    public int getRow() {
+        return row;
+    }
+
+    @Override
+    public String toString(){
+        return "[ " + row + ", " + col + " ]";
+    }
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj){
+            return true;
+        }
+
+        if(obj instanceof  Position){
+            Position pos = (Position) obj;
+            return row == pos.getRow() && col == pos.getCol();
+        }
+        else{
+            return false;
+        }
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
+}
